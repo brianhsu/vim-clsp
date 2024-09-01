322e77560004c54584753c09267e594e19292400
Brian Hsu <brianhsu.hsu@gmail.com>
2024-09-01 19:08:53 +0800

    modify: refactor code.

--- lua/clsp/floating_document_viewer.lua
+++ lua/clsp/floating_document_viewer.lua
@@ -9,15 +9,59 @@ M.config = {
         border = "rounded",
     },
     keymap = {
-        scroll_line_up = 'a',
-        scroll_line_down = 'b',
-        scroll_page_up = 'c',
-        scroll_page_down = 'd',
-        focus = 'e',
-        close = 'f'
+        scroll_line_up = '<C-y>',
+        scroll_line_down = '<C-e>',
+        scroll_page_up = '<C-b>',
+        scroll_page_down = '<C-f>',
+        focus = '<CR>',
+        close = 'q'
     }
 }
 
+local function setup_key_map(float_win_id, parent_win_id)
+    vim.keymap.set(
+        'n',
+        M.config.keymap.scroll_line_down,
+        util.run_normal_mode_command(float_win_id, parent_win_id, '<C-e>'),
+        {desc = 'Scroll document window 1 line down.', buffer = true}
+    )
+
+    vim.keymap.set(
+        'n',
+        M.config.keymap.scroll_line_up,
+        util.run_normal_mode_command(float_win_id, parent_win_id, '<C-y>'),
+        {desc = 'Scroll document window 1 line up.', buffer = true}
+    )
+
+    vim.keymap.set(
+        'n',
+        M.config.keymap.scroll_page_down,
+        util.run_normal_mode_command(float_win_id, parent_win_id, '<C-f>'),
+        {desc = 'Scroll document window 1 page down.', buffer = true}
+    )
+
+    vim.keymap.set(
+        'n',
+        M.config.keymap.scroll_page_up,
+        util.run_normal_mode_command(float_win_id, parent_win_id, '<C-b>'),
+        {desc = 'Scroll document window 1 page up.', buffer = true}
+    )
+
+    vim.keymap.set(
+        'n',
+        M.config.keymap.close,
+        util.run_normal_mode_command(float_win_id, parent_win_id, 'q'),
+        {desc = 'Close document window.', buffer = true}
+    )
+
+    vim.keymap.set(
+        'n',
+        M.config.keymap.focus,
+        util.focus_floating_window(float_win_id),
+        {desc = 'Focus document window.', buffer = true}
+    )
+
+end
 
 function M.floating_document_viewer(_, result, ctx, config)
     local parent_buf_id = vim.api.nvim_get_current_buf()
@@ -25,13 +69,7 @@ function M.floating_document_viewer(_, result, ctx, config)
 
     local float_buf_id, float_win_id = vim.lsp.handlers.hover(_, result, ctx, config)
 
-    vim.keymap.set('n', 'a', util.run_normal_mode_command(float_win_id, parent_win_id, '<C-e>'), {desc = 'Scroll 1 line down.', buffer = true})
-    vim.keymap.set('n', 'b', util.run_normal_mode_command(float_win_id, parent_win_id, '<C-y>'), {desc = 'Scroll 1 line up.', buffer = true})
-    vim.keymap.set('n', 'c', util.run_normal_mode_command(float_win_id, parent_win_id, '<C-f>'), {desc = 'Scroll 1 page down.', buffer = true})
-    vim.keymap.set('n', 'd', util.run_normal_mode_command(float_win_id, parent_win_id, '<C-b>'), {desc = 'Scroll 1 page up.', buffer = true})
-    vim.keymap.set('n', 'e', util.run_normal_mode_command(float_win_id, parent_win_id, 'q'), {desc = 'Scroll 1 page up.', buffer = true})
-    vim.keymap.set('n', 'i', util.focus_floating_window(float_win_id), {desc = 'Focus document window.', buffer = true})
-
+    setup_key_map(float_win_id, parent_win_id)
     util.create_unmap_key_autocmds(float_buf_id, float_win_id, parent_buf_id, M.config.keymap)
 
     vim.api.nvim_set_option_value('winblend', M.config.win_blend, {scope = 'local', win = float_win_id})
